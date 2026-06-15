f4dd65f41685839d032061b941accc8259b15157
Not-Juicy <jisujuicy@gmail.com>
2026-06-15 12:31:02 +0700

    Update Appwrite project ID and API key

--- src/appwriteClient.ts
+++ src/appwriteClient.ts
@@ -3,7 +3,7 @@ import { Client, Account, ID } from 'appwrite';
 const cfg = (window as any).__APPWRITE__ || {};
 const client = new Client()
   .setEndpoint(cfg.endpoint || 'https://sgp.cloud.appwrite.io/v1')
-  .setProject(cfg.projectId || '6a1d2f3b002adfa34f7a');
+  .setProject(cfg.projectId || '6a2a5cce00273365afb7');
 
 export const account = new Account(client);
 export { ID };
